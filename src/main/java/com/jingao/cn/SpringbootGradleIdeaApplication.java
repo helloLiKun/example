5f3d8cd1089d157361ae9be027d8c6adca1bc356
helloLiKun <13923813589@163.com>
2017-09-22 16:01:04 +0800

    1

---
+++ src/main/java/com/jingao/cn/SpringbootGradleIdeaApplication.java
@@ -0,0 +1,14 @@
+package com.jingao.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+public class SpringbootGradleIdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradleIdeaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jingao/cn/SpringbootGradleIdeaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jingao.cn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootGradleIdeaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
