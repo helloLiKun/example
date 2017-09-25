e7837f6381350473adf50b753d29a780649df683
liKun <13923813589@163.com>
2017-09-25 17:52:16 +0800

    9 25 5 52

---
+++ src/test/java/com/jingao/cn/dao/T_RedisDao.java
@@ -0,0 +1,32 @@
+package com.jingao.cn.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Administrator on 2017/9/25 0025.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class T_RedisDao {
+    public static Logger logger= LoggerFactory.getLogger(T_RedisDao.class);
+    @Test
+    public void contextLoads() {
+    }
+
+    @Autowired
+    RedisDao redisDao;
+    @Test
+    public void testRedis(){
+        redisDao.setKey("name","forezp");
+        redisDao.setKey("age","11");
+        redisDao.setKey("sal","3000");
+        logger.info(redisDao.getValue("height"));
+        logger.info(redisDao.getValue("age"));
+    }
+}
